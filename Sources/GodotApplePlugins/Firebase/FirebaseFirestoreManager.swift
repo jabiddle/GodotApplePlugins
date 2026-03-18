86293cdd6f860d206444b3971db6c005c9f053f4
jabiddle <dev@jabiddle.com>
2026-03-18 08:19:47 -0400

    Refactor Common Firebase Type Handling

--- Sources/GodotApplePlugins/Firebase/FirebaseFirestoreManager.swift
+++ Sources/GodotApplePlugins/Firebase/FirebaseFirestoreManager.swift
@@ -13,73 +13,11 @@ import FirebaseFirestore
 class FirebaseFirestoreManager: RefCounted, @unchecked Sendable {
     
     @Signal("collection", "document", "data") var document_read: SignalWithArguments<String, String, VariantDictionary>
-    @Signal("error") var document_error: SignalWithArguments<String>
     @Signal("collection", "document_id") var document_added: SignalWithArguments<String, String>
     @Signal("collection", "documents") var collection_read: SignalWithArguments<String, VariantArray>
     @Signal("collection", "document") var update_completed: SignalWithArguments<String, String>
-    
-    private func variantToAny(_ variant: Variant) -> Any {
-        switch variant.gtype {
-        case .`nil`: return NSNull()
-        case .int: return Int(variant) ?? 0
-        case .float: return Double(variant) ?? 0.0
-        case .bool: return Bool(variant) ?? false
-        case .string: return String(variant) ?? ""
-        case .dictionary:
-            let gDict = VariantDictionary(variant)
-            var swiftDict: [String: Any] = [:]
-            for key in gDict.keys() {
-                if let val = gDict[key] {
-                    swiftDict[String(key)] = variantToAny(val)
-                }
-            }
-            return swiftDict
-        case .array:
-            let gArray = VariantArray(variant)
-            var swiftArray: [Any] = []
-            for i in 0..<Int(gArray.count) {
-                swiftArray.append(variantToAny(gArray[Int64(i)]))
-            }
-            return swiftArray
-        case .packedByteArray:
-            return PackedByteArray(variant).asData() ?? Data()
-        default: return variant.description
-        }
-    }
-    
-    private func anyToVariant(_ value: Any) -> Variant {
-        if value is NSNull { return Variant() }
-        if let intVal = value as? Int { return Variant(intVal) }
-        if let doubleVal = value as? Double { return Variant(doubleVal) }
-        if let boolVal = value as? Bool { return Variant(boolVal) }
-        if let stringVal = value as? String { return Variant(stringVal) }
-        if let timestamp = value as? Timestamp { return Variant(timestamp.dateValue().timeIntervalSince1970) }
-        if let geoPoint = value as? GeoPoint {
-            let gDict = VariantDictionary()
-            gDict[Variant("latitude")] = Variant(geoPoint.latitude)
-            gDict[Variant("longitude")] = Variant(geoPoint.longitude)
-            return Variant(gDict)
-        }
-        if let docRef = value as? DocumentReference { return Variant(docRef.path) }
-        if let dateVal = value as? Date { return Variant(dateVal.timeIntervalSince1970) }
-        if let dataVal = value as? Data { return Variant(dataVal.toPackedByteArray()) }
-        if let dictVal = value as? [String: Any] {
-            let gDict = VariantDictionary()
-            for (k, v) in dictVal {
-                gDict[Variant(k)] = anyToVariant(v)
-            }
-            return Variant(gDict)
-        }
-        if let arrayVal = value as? [Any] {
-            let gArray = VariantArray()
-            for v in arrayVal {
-                gArray.append(value: anyToVariant(v))
-            }
-            return Variant(gArray)
-        }
-        return Variant(String(describing: value))
-    }
-    
+    @Signal("error") var document_error: SignalWithArguments<String>
+
     @Callable
     func get_document(collection: String, document: String) {
         let db = Firestore.firestore()
@@ -88,7 +26,7 @@ class FirebaseFirestoreManager: RefCounted, @unchecked Sendable {
             if let documentSnap = documentSnap, documentSnap.exists, let data = documentSnap.data() {
                 let gDict = VariantDictionary()
                 for (key, value) in data {
-                    gDict[Variant(key)] = self.anyToVariant(value)
+                    gDict[Variant(key)] = FirebaseVariantConverter.anyToVariant(value)
                 }
                 DispatchQueue.main.async { self.document_read.emit(collection, document, gDict) }
             } else {
@@ -110,7 +48,7 @@ class FirebaseFirestoreManager: RefCounted, @unchecked Sendable {
                     let gDict = VariantDictionary()
                     gDict[Variant("id")] = Variant(documentSnap.documentID)
                     for (key, value) in documentSnap.data() {
-                        gDict[Variant(key)] = self.anyToVariant(value)
+                        gDict[Variant(key)] = FirebaseVariantConverter.anyToVariant(value)
                     }
                     results.append(value: Variant(gDict))
                 }
@@ -124,7 +62,7 @@ class FirebaseFirestoreManager: RefCounted, @unchecked Sendable {
         var props: [String: Any] = [:]
         for key in data.keys() {
             let k = String(key)
-            if let val = data[key] { props[k] = variantToAny(val) }
+            if let val = data[key] { props[k] = FirebaseVariantConverter.variantToAny(val) }
         }
         let db = Firestore.firestore()
         var ref: DocumentReference? = nil
@@ -143,7 +81,7 @@ class FirebaseFirestoreManager: RefCounted, @unchecked Sendable {
         var props: [String: Any] = [:]
         for key in data.keys() {
             let k = String(key)
-            if let val = data[key] { props[k] = variantToAny(val) }
+            if let val = data[key] { props[k] = FirebaseVariantConverter.variantToAny(val) }
         }
         let db = Firestore.firestore()
         db.collection(collection).document(document).setData(props) { [weak self] error in
@@ -161,7 +99,7 @@ class FirebaseFirestoreManager: RefCounted, @unchecked Sendable {
         var props: [String: Any] = [:]
         for key in data.keys() {
             let k = String(key)
-            if let val = data[key] { props[k] = variantToAny(val) }
+            if let val = data[key] { props[k] = FirebaseVariantConverter.variantToAny(val) }
         }
         let db = Firestore.firestore()
         db.collection(collection).document(document).updateData(props) { [weak self] error in
---
+++ Sources/GodotApplePlugins/Firebase/FirebaseVariantConverter.swift
@@ -0,0 +1,74 @@
+//
+//  FirebaseVariantConverter.swift
+//  GodotApplePlugins
+//
+//  Created by Jacob Biddle on 3/18/26.
+//
+
+import Foundation
+@preconcurrency import SwiftGodotRuntime
+import FirebaseFirestore
+
+enum FirebaseVariantConverter {
+    static func variantToAny(_ variant: Variant) -> Any {
+        switch variant.gtype {
+        case .`nil`: return NSNull()
+        case .int: return Int(variant) ?? 0
+        case .float: return Double(variant) ?? 0.0
+        case .bool: return Bool(variant) ?? false
+        case .string: return String(variant) ?? ""
+        case .dictionary:
+            let gDict = VariantDictionary(variant)
+            var swiftDict: [String: Any] = [:]
+            for key in gDict.keys() {
+                if let val = gDict[key] {
+                    swiftDict[String(key)] = variantToAny(val)
+                }
+            }
+            return swiftDict
+        case .array:
+            let gArray = VariantArray(variant)
+            var swiftArray: [Any] = []
+            for i in 0..<Int(gArray.count) {
+                swiftArray.append(variantToAny(gArray[Int64(i)]))
+            }
+            return swiftArray
+        case .packedByteArray:
+            return PackedByteArray(variant).asData() ?? Data()
+        default: return variant.description
+        }
+    }
+    
+    static func anyToVariant(_ value: Any) -> Variant {
+        if value is NSNull { return Variant() }
+        if let intVal = value as? Int { return Variant(intVal) }
+        if let doubleVal = value as? Double { return Variant(doubleVal) }
+        if let boolVal = value as? Bool { return Variant(boolVal) }
+        if let stringVal = value as? String { return Variant(stringVal) }
+        if let timestamp = value as? Timestamp { return Variant(timestamp.dateValue().timeIntervalSince1970) }
+        if let geoPoint = value as? GeoPoint {
+            let gDict = VariantDictionary()
+            gDict[Variant("latitude")] = Variant(geoPoint.latitude)
+            gDict[Variant("longitude")] = Variant(geoPoint.longitude)
+            return Variant(gDict)
+        }
+        if let docRef = value as? DocumentReference { return Variant(docRef.path) }
+        if let dateVal = value as? Date { return Variant(dateVal.timeIntervalSince1970) }
+        if let dataVal = value as? Data { return Variant(dataVal.toPackedByteArray()) }
+        if let dictVal = value as? [String: Any] {
+            let gDict = VariantDictionary()
+            for (k, v) in dictVal {
+                gDict[Variant(k)] = anyToVariant(v)
+            }
+            return Variant(gDict)
+        }
+        if let arrayVal = value as? [Any] {
+            let gArray = VariantArray()
+            for v in arrayVal {
+                gArray.append(value: anyToVariant(v))
+            }
+            return Variant(gArray)
+        }
+        return Variant(String(describing: value))
+    }
+}
